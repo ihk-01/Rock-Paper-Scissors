60a11225674bc19ee4c16dfd3a98d089ffff86fd
ihk-01 <ingridkilmer@gmail.com>
2023-01-25 19:56:17 -0500

    added number of rounds played

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 let youScore = 0;
 let player2Score = 0;
+let numOfRounds = prompt('How many rounds should we play?',11);
+let numRounds = (numOfRounds != 0) ? numOfRounds : 11;
+let currRound = 0;
+const round_label_div = document.getElementById("round-label");
 const result_p = document.querySelector(".result > p");
 const score_div = document.querySelector(".score");
 const youScore_span = document.getElementById("youScore");
@@ -19,6 +23,8 @@ function win(you, player2) {
     youScore_span.textContent = youScore;
     player2Score_span.textContent = player2Score;
     result_p.textContent = toCapitalize(you) + " beats " + toCapitalize(player2) + ". You Win!!!"
+    currRound++
+    round_label_div.textContent = `Round: ${currRound} / ${numRounds}`;
 }
 
 function lose(you, player2) {
@@ -26,16 +32,21 @@ function lose(you, player2) {
     player2Score_span.textContent = player2Score;
     youScore_span.textContent = youScore;
     result_p.textContent = toCapitalize(you) + " loses to " + toCapitalize(player2) + ". You Lost!!!"
+    currRound++;
+    round_label_div.textContent = `Round: ${currRound} / ${numRounds}`;
 }
 
 function draw(you, player2) {
     result_p.textContent = toCapitalize(you) + " equals " + toCapitalize(player2) + ". It's a draw!!!"
+    currRound++;
+    round_label_div.textContent = `Round: ${currRound} / ${numRounds}`;
 }
 
 function getP2Choice() {
     const choices = ['rock', 'paper', 'scissors'];
     const randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
+    
 }
 
 function play(youChoice) {
@@ -57,9 +68,14 @@ function play(youChoice) {
         case "scissorsscissors":
             draw(youChoice, p2Choice);
             break;
-        
-}  
+            
+    }  
+    if (currRound == numRounds) {
+        alert("That's how many rounds you wanted to play! Game over!");
+    }
+    
 }
+
 function main() {
     rock_div.addEventListener('click', function() {
         play("rock");
@@ -73,5 +89,6 @@ function main() {
         play("scissors");
 })
 }
+
 main();
 
